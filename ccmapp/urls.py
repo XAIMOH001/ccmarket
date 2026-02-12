1ed1c446a92a4490451de3131c6dcf7db031b360
XAIMOH001 <xaimohchau@gmail.com>
2026-02-12 19:00:37 +0300

    opened admin page

--- ccmapp/urls.py
+++ ccmapp/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from ccmapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('User/', views.user, name='user'),
     path('categories/', views.categories, name='categories'),
